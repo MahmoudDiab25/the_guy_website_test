e72f6430983a5d48086c998963184dd71b60d463
mahmoud diab <mahmouddiab0225@gmail.com>
2022-11-20 23:19:37 +0200

    move scroll bar to left

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -2,21 +2,33 @@ import React, { useState } from "react";
 import style from "./style.module.scss";
 
 function footer() {
-  const [avtive, setAvtice] = useState(false);
+  const [active, setAvtice] = useState(false);
+
   return (
-    <div className={style.conatiner}>
+    <div className={[style.conatiner, active && style.goUp].join(" ")}>
       <div className={style.innerContainer}>
-        <div className={style.title}>
-          <span className={style.text}> לבחירת מוצר להצטרפות</span>
+        <div className={style.title} onClick={() => setAvtice(!active)}>
+          <span className={style.text}>לבחירת מוצר להצטרפות</span>
           <span>
             <i
-              className={[style.arrow, avtive ? style.down : style.up].join(
+              className={[style.arrow, active ? style.down : style.up].join(
                 " "
               )}
             ></i>
           </span>
         </div>
-        <div className={style.buttoms}></div>
+        <div className={style.buttoms}>
+          <select className={style.customSelect}>
+            <option value="מיטב דש טרייד">מיטב דש טרייד</option>
+            <option value="מיטב דש גמל להשקעה">מיטב דש גמל להשקעה</option>
+            <option value="מיטב דש מנג'ר">מיטב דש מנג'ר</option>
+            <option value="מיטב דש ניהול תיק השקעות">
+              מיטב דש ניהול תיק השקעות
+            </option>
+            <option value="מיטב דש קידס">מיטב דש קידס</option>
+          </select>
+          <span>להצטרפות אונליין</span>
+        </div>
       </div>
     </div>
   );
