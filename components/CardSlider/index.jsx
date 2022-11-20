de7d46951891ad90ba0b80c477e509dec3d20d07
Mahmoud Diab <mahmouddiab0225@gmail.com>
2022-11-21 00:20:29 +0200

    Update index.jsx

--- components/CardSlider/index.jsx
+++ components/CardSlider/index.jsx
@@ -47,7 +47,7 @@ const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
-    items: 4,
+    items: 5,
   },
   desktop: {
     breakpoint: { max: 3000, min: 1200 },
