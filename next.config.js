e57e06b546d1cd6aa11682f06aa04fd34ca2ad73
Mahmoud Diab <mahmouddiab0225@gmail.com>
2022-11-20 18:16:31 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  resolve: {
+    extensions: ["", ".js", ".jsx"],
+  },
+};
+
+module.exports = nextConfig;
