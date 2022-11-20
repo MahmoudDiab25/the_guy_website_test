2d259eab039d5cc9dbedb9b6da8d93257668a9cb
mahmoud diab <mahmouddiab0225@gmail.com>
2022-11-21 00:09:51 +0200

    add animation to nav bar

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -22,38 +22,32 @@ function Header() {
           </div>
         </div>
       </div>
-      {showNav && (
-        <div
-          className={[
-            style.showNav,
-            showNav ? style.fadeIn : style.fadeOut,
-          ].join(" ")}
-        >
-          <div className={style.title}>
-            <div onClick={() => setShowNav(false)}>
-              <img
-                src="https://flaticons.net/icon.php?slug_category=office&slug_icon=wrong"
-                alt=""
-                className={style.closeButton}
-              />
-            </div>
-            <div className={style.logo}>
-              <img
-                src="https://lps.meitav.co.il/images/available-money/logo_white.png"
-                alt=""
-                width="137px"
-              />
-            </div>
+
+      <div className={[style.showNav, showNav && style.fadeIn].join(" ")}>
+        <div className={style.title}>
+          <div onClick={() => setShowNav(false)}>
+            <img
+              src="https://flaticons.net/icon.php?slug_category=office&slug_icon=wrong"
+              alt=""
+              className={style.closeButton}
+            />
           </div>
-          <div className={style.pageButtons}>
-            <span className={style.button}>מיטב דש טרייד </span>
-            <span className={style.button}>מיטב דש גמל להשקעה </span>
-            <span className={style.button}>מיטב דש מנג'ר </span>
-            <span className={style.button}>מיטב דש ניהול תיק השקעות </span>
-            <span className={style.button}>מיטב דש קידס </span>
+          <div className={style.logo}>
+            <img
+              src="https://lps.meitav.co.il/images/available-money/logo_white.png"
+              alt=""
+              width="137px"
+            />
           </div>
         </div>
-      )}
+        <div className={style.pageButtons}>
+          <span className={style.button}>מיטב דש טרייד </span>
+          <span className={style.button}>מיטב דש גמל להשקעה </span>
+          <span className={style.button}>מיטב דש מנג'ר </span>
+          <span className={style.button}>מיטב דש ניהול תיק השקעות </span>
+          <span className={style.button}>מיטב דש קידס </span>
+        </div>
+      </div>
     </div>
   );
 }
